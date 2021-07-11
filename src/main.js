112c4bb6ed17292342ab43a14c07255cc88a6188
Adam Syarif Hidayatullah <adamsyarif219@gmail.com>
2021-07-11 07:19:27 +0700

    update skin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+
+// --------- Prime Vue ---------- //
+import PrimeVue from "primevue/config";
+
+import Card from "primevue/card"
+import InputText from "primevue/inputtext";
+import Button from 'primevue/button';
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
+import 'primeflex/primeflex.css'
+// ------------------------------ //
+
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+
+// --------- Prime Vue ---------- //
+app.use(PrimeVue)
+
+app.component('Card', Card)
+app.component('Button', Button)
+app.component('InputText', InputText)
+// ------------------------------ //
+
+app.mount('#app')
---
+++ src/views/HomePage.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="p-grid p-ai-center vertical-container">
+
+    <div class="p-col">
+      <div class="p-d-flex p-jc-center">
+
+        <div class="box-divider"></div>
+        <img src="../assets/logo2.png" class="logo-image p-my-auto">
+        <div class="box-divider"></div>
+
+        <Card class="card-authentication">
+          <template #title>
+            <div class="p-text-center p-text-light text-white p-mt-4" style="font-size: 30px">Welcome to</div>
+            <div class="p-text-center p-text-bold text-white p-mt-2"  style="font-size: 28px">seafarindo.online</div>
+          </template>
+          <template #content>
+            <div class="p-grid p-jc-center">
+              <div class="p-col-12">
+                <div class="p-d-flex p-jc-center">
+                  <Button style="background-color: #0994be" class="button-authentication p-mt-3" label="Sign In" @click="$router.push('/sign-in')" />
+                </div>
+              </div>
+              <div class="p-col-12">
+                <div class="p-d-flex p-jc-center">
+                  <Button label="Sign Up" style="background-color: #a5e0f3" class="button-authentication p-mt-2"/>
+                </div>
+              </div>
+            </div>
+          </template>
+        </Card>
+
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<style scoped lang="scss">
+.vertical-container {
+  height: 100vh;
+}
+
+@media only screen and (max-width: 1200px) {
+  .logo-image, .box-divider {
+    display: none;
+  }
+}
+
+.logo-image {
+  height: 300px;
+  width: 300px;
+}
+
+.box-divider {
+  width: 120px;
+}
+
+.card-authentication {
+  background-color: #203572;
+  margin: 0 5em 0 5em;
+  border-radius: 25px;
+  height: 320px;
+  width: 420px;
+}
+
+.text-white {
+  color: white;
+}
+
+.button-authentication{
+  width: 90%;
+  height: 43px;
+}
+</style>
