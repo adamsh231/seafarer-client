40834bf6c1befa3eb108f273cb6ab8ed18f1fd8d
Adam Syarif Hidayatullah <adamsyarif219@gmail.com>
2021-08-02 13:01:34 +0700

    update

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -4,7 +4,7 @@
     <template v-slot:content>
       <div class="p-grid p-jc-center">
         <div class="p-col-12">
-          <div class="p-text-center p-text-bold text-white p-mt-4" style="font-size: 40px">Server Error Brai</div>
+          <div class="p-text-center p-text-bold text-white p-mt-4" style="font-size: 40px">Server Error</div>
           <div class="p-text-center p-text-bold text-white p-mt-4" style="font-size: 40px">500</div>
         </div>
       </div>
